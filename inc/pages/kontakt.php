b9679470a6e69705194234b4cf92cbc6699dee07
vaddi <mvattersen@gmail.com>
2023-10-17 23:01:44 +0200

    Refractor config.pho.example File and Contacts Page

--- inc/pages/kontakt.php
+++ inc/pages/kontakt.php
@@ -20,61 +20,72 @@ $contact = array(
   'phone'       =>  CONTACTPHONE,
   'fax'         =>  CONTACTFAX,
   'mail'        =>  CONTACTMAIL,
-	'twitter'			=>  CONTACTTWITTER,
-	'facebook'		=>  CONTACTFACEBOOK,
-	'instagram'		=>  CONTACTINSTAGRAM,
-	'youtube'			=>  CONTACTYOUTUBE
+	'twitter'			=>  SOCIALTWITTER,
+	'facebook'		=>  SOCIALFACEBOOK,
+	'instagram'		=>  SOCIALINSTAGRAM,
+	'youtube'			=>  SOCIALYOUTUBE
 );
 ?>
 <h1>Kontakt</h1>
 
 <?php
 // google maps
-if( isset( $contact['street'] ) && isset( $contact['housenumber'] ) ) {
-  echo '<h2>Karte</h2>';
-  echo '<div class="gmap">';
-  require_once( 'inc/class/extensions/Base.php' );
-  echo Base::gmaps( $contact );
-  echo '</div>';
+if( CONTACTMAP ) {
+	if( isset( $contact['street'] ) && isset( $contact['housenumber'] ) ) {
+	  echo '<h2>Karte</h2>';
+	  echo '<div class="gmap">';
+	  require_once( 'inc/class/extensions/Base.php' );
+	  echo Base::gmaps( $contact );
+	  echo '</div>';
+	}
+	
 }
 
-echo '<div>';
-echo '<h2>Kontaktformular</h2>';
-include('inc/form/formular.php'); 
-echo '</div>';
+// Contact form
+if( CONTACTFORM ) {
+	echo '<div>';
+	echo '<h2>Kontaktformular</h2>';
+	include('inc/form/formular.php'); 
+	echo '</div>';
+	
+}
 
-echo '<div>';
-echo '<h2>Anschrift</h2>';
-// Name
-echo isset( $contact['name'] ) && $contact['name'] != "" ? $contact['name'] . ' ' : '';
-echo isset( $contact['surename'] ) && $contact['surename'] != "" ? $contact['surename'] : '';
-echo ( isset( $contact['name'] ) && $contact['name'] != "" ) || ( isset( $contact['surename'] ) && $contact['surename'] != "" ) ? "<br />\n" : '';
-// Street
-echo isset( $contact['street'] ) && $contact['street'] != "" ? $contact['street'] . ' ' : '';
-echo isset( $contact['housenumber'] ) && $contact['housenumber'] != "" ? $contact['housenumber'] : '';
-echo ( isset( $contact['street'] ) && $contact['street'] != "" ) || ( isset( $contact['housenumber'] ) && $contact['housenumber'] != "" ) ? "<br />\n" : '';
-// City
-echo isset( $contact['plz'] ) && $contact['plz'] != "" ? $contact['plz'] . ' ' : '';
-echo isset( $contact['city'] ) && $contact['city'] != "" ? $contact['city'] : '';
-echo ( isset( $contact['plz'] ) && $contact['plz'] != "" ) || ( isset( $contact['city'] ) && $contact['city'] != "" ) ? "<br />\n" : '';
-// State
-echo isset( $contact['state'] ) && $contact['state'] != "" ? $contact['state'] . "<br />\n" : '';
-// Land
-echo isset( $contact['land'] ) && $contact['land'] != "" ? $contact['land'] . "<br />\n" : '';
+if( CONTACTADRESS ) {
+	echo '<div>';
+	echo '<h2>Anschrift</h2>';
+	// Name
+	echo isset( $contact['name'] ) && $contact['name'] != "" ? $contact['name'] . ' ' : '';
+	echo isset( $contact['surename'] ) && $contact['surename'] != "" ? $contact['surename'] : '';
+	echo ( isset( $contact['name'] ) && $contact['name'] != "" ) || ( isset( $contact['surename'] ) && $contact['surename'] != "" ) ? "<br />\n" : '';
+	// Street
+	echo isset( $contact['street'] ) && $contact['street'] != "" ? $contact['street'] . ' ' : '';
+	echo isset( $contact['housenumber'] ) && $contact['housenumber'] != "" ? $contact['housenumber'] : '';
+	echo ( isset( $contact['street'] ) && $contact['street'] != "" ) || ( isset( $contact['housenumber'] ) && $contact['housenumber'] != "" ) ? "<br />\n" : '';
+	// City
+	echo isset( $contact['plz'] ) && $contact['plz'] != "" ? $contact['plz'] . ' ' : '';
+	echo isset( $contact['city'] ) && $contact['city'] != "" ? $contact['city'] : '';
+	echo ( isset( $contact['plz'] ) && $contact['plz'] != "" ) || ( isset( $contact['city'] ) && $contact['city'] != "" ) ? "<br />\n" : '';
+	// State
+	echo isset( $contact['state'] ) && $contact['state'] != "" ? $contact['state'] . "<br />\n" : '';
+	// Land
+	echo isset( $contact['land'] ) && $contact['land'] != "" ? $contact['land'] . "<br />\n" : '';
 
-echo "<br />\n";
-// Phnoe, Fax and Mail
-echo isset( $contact['phone'] ) && $contact['phone'] != "" ? '<a href="tel:' . $contact['phone'] . '">' . $contact['phone'] . "</a><br />\n" : '';
-echo isset( $contact['fax'] ) && $contact['fax'] != "" ? '<a href="fax:' . $contact['fax'] . '">' . $contact['fax'] . "</a><br />\n" : '';
-echo isset( $contact['mail'] ) && $contact['mail'] != "" ? '<a href="mailto:' . $contact['mail'] . '">' . $contact['mail'] . "</a><br />\n" : '';
-echo '</div>';
+	echo "<br />\n";
+	// Phnoe, Fax and Mail
+	echo isset( $contact['phone'] ) && $contact['phone'] != "" ? '<a href="tel:' . preg_replace('/[^a-zA-Z0-9-+_\.]/','', $contact['phone'] ) . '">' . $contact['phone'] . "</a><br />\n" : '';
+	echo isset( $contact['fax'] ) && $contact['fax'] != "" ? '<a href="fax:' . $contact['fax'] . '">' . $contact['fax'] . "</a><br />\n" : '';
+	echo isset( $contact['mail'] ) && $contact['mail'] != "" ? '<a href="mailto:' . $contact['mail'] . '">' . $contact['mail'] . "</a><br />\n" : '';
+	echo '</div>';
+}
 
-echo '<div>';
-echo '<h2>Links</h2>';
-echo isset( $contact['twitter'] ) || $contact['twitter'] == "https://twitter.com/" ? '' : '<a href="' . $contact['twitter'] . '">' . $contact['twitter'] . "</a><br />\n";
-echo isset( $contact['facebook'] ) || $contact['twitter'] == "https://twitter.com/" ? '' : '<a href="' . $contact['facebook'] . '">' . $contact['facebook'] . "</a><br />\n";
-echo isset( $contact['instagram'] ) || $contact['instagram'] == "https://www.instagram.com/" ? '' : '<a href="' . $contact['instagram'] . '">' . $contact['instagram'] . "</a><br />\n";
-echo isset( $contact['youtube'] )  || $contact['youtube'] == "https://www.youtube.com/" ? '' : '<a href="' . $contact['youtube'] . '">' . $contact['youtube'] . "</a><br />\n";
-echo '</div>';
+if( SOCIALLINKS ) {
+	echo '<div>';
+	echo '<h2>Links</h2>';
+	echo isset( $contact['twitter'] ) && $contact['twitter'] != "https://twitter.com/" && $contact['twitter'] != "" ? 'Twitter: <a href="' . $contact['twitter'] . '">' . $contact['twitter'] . "</a><br />\n" : '';
+	echo isset( $contact['facebook'] ) && $contact['facebook'] != "https://facebook.com/" && $contact['facebook'] != "" ? 'Facebook: <a href="' . $contact['facebook'] . '">' . $contact['facebook'] . "</a><br />\n" : '';
+	echo isset( $contact['instagram'] ) && $contact['instagram'] != "https://www.instagram.com/" && $contact['instagram'] != "" ? 'Instagram: <a href="' . $contact['instagram'] . '">' . $contact['instagram'] . "</a><br />\n" : '';
+	echo isset( $contact['youtube'] ) && $contact['youtube'] != "https://www.youtube.com/" && $contact['youtube'] != "" ? 'YouTube: <a href="' . $contact['youtube'] . '">' . $contact['youtube'] . "</a><br />\n" : '';
+	echo '</div>';
+}
 
 ?>
